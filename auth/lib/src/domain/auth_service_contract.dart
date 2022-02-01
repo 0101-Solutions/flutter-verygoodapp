d7fcdfd1c5e96b269d9911d8d689faf69f889810
Toxic254 <cyrus_g@outlook.com>
2022-02-01 12:19:05 +0300

    Created our AuthService abstract class to lay out methods needed for the concrete class.

---
+++ auth/lib/src/domain/auth_service_contract.dart
@@ -0,0 +1,12 @@
+import 'package:async/async.dart';
+
+import 'package:auth/src/constants/token.dart';
+
+/// Our base class for the authentication service.
+abstract class IAuthService {
+  /// The signIn() method that will sign users in with either email or google.
+  Future<Result<Token>> signIn();
+
+  /// The signOut() method, that signs users out of the app.
+  Future<void> signOut();
+}
