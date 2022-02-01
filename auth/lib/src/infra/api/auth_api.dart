943e7a280724e53e1799a545e2d025eb22ba273f
Toxic254 <cyrus_g@outlook.com>
2022-02-01 12:22:44 +0300

    Implemented our SignIn and SignUp API call methods. Still needs a refactor.

---
+++ auth/lib/src/infra/api/auth_api.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: avoid_dynamic_calls
+import 'dart:convert';
+
+import 'package:async/async.dart';
+import 'package:auth/src/constants/credential.dart';
+import 'package:auth/src/infra/api/auth_api_contract.dart';
+import 'package:auth/src/infra/api/helpers/mapper.dart';
+
+import 'package:http/http.dart' as http;
+
+/// This class is responsible for interacting with the API and call the
+/// methods defined in the `IAuthAPI` contract/abstract class.
+class AuthApi implements IAuthApi {
+  /// Injected the http.Client instance into our AuthAPI constructor.
+  AuthApi(this.baseUrl, this._client);
+
+  final http.Client _client;
+  String baseUrl;
+
+  @override
+  Future<Result<String>> signIn(Credential credential) async {
+    final endpoint = Uri.parse('$baseUrl/auth/signin');
+    return _postCredential(endpoint, credential);
+  }
+
+  @override
+  Future<Result<String>> signUp(Credential credential) async {
+    final endpoint = Uri.parse('$baseUrl/auth/signup');
+    return _postCredential(endpoint, credential);
+  }
+
+  Future<Result<String>> _postCredential(
+    Uri endpoint,
+    Credential credential,
+  ) async {
+    final response = await _client.post(
+      endpoint,
+      headers: {'Content-type': 'application/json'},
+      body: Mapper.toJson(credential),
+    );
+
+    if (response.statusCode != 200) {
+      return Result.error('Server error. Try Again.');
+    }
+
+    final dynamic json = jsonDecode(response.body);
+
+    return json['auth_token'] != null
+        ? Result.value(json!['auth_token'] as String)
+        : Result.error(json['message'] as String);
+  }
+}
