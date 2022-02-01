c931ec00f14fb45d0d5f3c62292a7821202a0fcd
Toxic254 <cyrus_g@outlook.com>
2022-02-01 12:21:31 +0300

    Our GoogleAuth class which is responsible for authenticating users in our app using GoogleSignIn.

---
+++ auth/lib/src/infra/adapters/google_auth.dart
@@ -0,0 +1,55 @@
+import 'package:async/async.dart';
+import 'package:auth/src/constants/credential.dart';
+
+import 'package:auth/src/constants/token.dart';
+import 'package:auth/src/domain/auth_service_contract.dart';
+import 'package:auth/src/infra/api/auth_api_contract.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+/// GoogleAuth Class to help with authenticating users into our app using
+/// Google Sign In.
+class GoogleAuth implements IAuthService {
+  /// Inject GoogleSignIn instance by default in the constructor.
+  /// This is done for testing purposes we can inject a MockGoogleSignIn.
+  GoogleAuth(this._authApi, [GoogleSignIn? googleSignIn])
+      : _googleSignIn = googleSignIn ??
+            GoogleSignIn(
+              scopes: ['email', 'profile'],
+            );
+
+  final IAuthApi _authApi;
+  final GoogleSignIn _googleSignIn;
+  late GoogleSignInAccount _currentUser;
+
+  @override
+  Future<Result<Token>> signIn() async {
+    await _handleGoogleSignIn();
+
+    final credential = Credential(
+      type: AuthType.google,
+      email: _currentUser.email,
+      name: _currentUser.displayName,
+    );
+
+    final result = await _authApi.signIn(credential);
+
+    if (result.isError) {
+      return result.asError;
+    }
+
+    return Result.value(Token(result.asValue!.value));
+  }
+
+  @override
+  Future<void> signOut() async {
+    await _googleSignIn.disconnect();
+  }
+
+  Future _handleGoogleSignIn() async {
+    try {
+      _currentUser = (await _googleSignIn.signIn())!;
+    } catch (e) {
+      return;
+    }
+  }
+}
