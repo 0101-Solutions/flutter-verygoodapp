374411e42aacf209e1c52e19bd341aa5f5b6d080
Toxic254 <cyrus_g@outlook.com>
2022-02-01 12:22:09 +0300

    Created our blueprint IAuthApi containing methods to be called by the AuthApi.

---
+++ auth/lib/src/infra/api/auth_api_contract.dart
@@ -0,0 +1,22 @@
+import 'package:async/async.dart';
+import 'package:auth/src/constants/credential.dart';
+
+/// Base class containing the methods needed to interact with our external API:
+/// - signIn method call to login existing users to the app.
+/// - signUp method call to signUp/onboard users on to the app.
+///
+/// ```dart
+///
+/// Future<Result<String>> signIn(Credential credential);
+///
+/// Future<Result<String>> signUp(Credential credential);
+///
+/// ```
+///
+abstract class IAuthApi {
+  /// signUp method call to the external API - takes the user `Credential`.
+  Future<Result<String>> signIn(Credential credential);
+
+  /// signUp method call to the external API - takes the user `Credential`.
+  Future<Result<String>> signUp(Credential credential);
+}
