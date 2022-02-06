18dee6f3d1ca87aa5a3390b9f241ec300b8456de
Toxic254 <cyrus_g@outlook.com>
2022-02-06 23:11:06 +0300

    Refactored our mapper to align with our backend values.

--- auth/lib/src/infra/api/helpers/mapper.dart
+++ auth/lib/src/infra/api/helpers/mapper.dart
@@ -3,13 +3,13 @@ import 'package:auth/src/constants/credential.dart';
 /// A class with static methods that returns JSON format
 /// from properties passed, here being credential metadata.
 class Mapper {
-  /// This staic method converts the Credential model to JSON,
+  /// This static method converts the Credential model to JSON,
   /// before it is sent to the server.
   static Map<String, dynamic> toJson(Credential credential) =>
       <String, dynamic>{
-        'type': credential.type,
+        'auth_type': credential.type.toString().split('.').last,
         'email': credential.email,
         'name': credential.name,
-        'password': credential.password,
+        'password': credential.password
       };
 }
