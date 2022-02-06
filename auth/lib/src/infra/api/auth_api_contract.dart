733bdfb018aedc570f3b0eba5cda8c205ac8ada5
Toxic254 <cyrus_g@outlook.com>
2022-02-06 23:09:45 +0300

    Corrected documentation and added signOut()

--- auth/lib/src/infra/api/auth_api_contract.dart
+++ auth/lib/src/infra/api/auth_api_contract.dart
@@ -1,5 +1,6 @@
 import 'package:async/async.dart';
 import 'package:auth/src/constants/credential.dart';
+import 'package:auth/src/constants/token.dart';
 
 /// Base class containing the methods needed to interact with our external API:
 /// - signIn() to login existing users to the app.
@@ -14,9 +15,15 @@ import 'package:auth/src/constants/credential.dart';
 /// ```
 ///
 abstract class IAuthApi {
-  /// signIn method call to the external API - takes the user `Credential`.
+  /// signIn method call to the external API - takes the user `Credential` and
+  /// returns a JWT `Token` Object.
   Future<Result<String>> signIn(Credential credential);
 
-  /// signUp method call to the external API - takes the user `Credential`.
+  /// signUp method call to the external API - takes the user `Credential` and
+  /// returns a JWT `Token` Object.
   Future<Result<String>> signUp(Credential credential);
+
+  /// signOut method call to the external API - takes the stored `Token` and
+  /// checks whether its valid or not to log the user out.
+  Future<Result<bool>> signOut(Token token);
 }
