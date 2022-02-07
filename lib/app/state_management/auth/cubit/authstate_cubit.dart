fd7eeaa36f1a433b1e07bfd76fe01f0a6a99e8f9
Toxic254 <cyrus_g@outlook.com>
2022-02-07 11:57:43 +0300

    Added our signOut() and signUp() cubit functions and their appropriate states.

--- lib/app/state_management/auth/cubit/authstate_cubit.dart
+++ lib/app/state_management/auth/cubit/authstate_cubit.dart
@@ -2,15 +2,18 @@ import 'package:async/async.dart';
 import 'package:auth/auth.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:verygoodapp/app/cache/local_store_contract.dart';
+import 'package:verygoodapp/app/data/models/user.dart';
 
 part 'authstate_state.dart';
 
 class AuthstateCubit extends Cubit<AuthState> {
-  AuthstateCubit() : super(AuthStateInitial());
+  AuthstateCubit(this.localStore) : super(AuthStateInitial());
+
+  final ILocalStore localStore;
 
   /// This is the signIn() that takes the IAuthService to
   /// interact with our module and present according states.
-
   Future signIn(IAuthService authService) async {
     _startLoading();
 
@@ -19,7 +22,31 @@ class AuthstateCubit extends Cubit<AuthState> {
     _setResultOfAuthState(result);
   }
 
-  Future signOut(IAuthService authService) async {}
+  Future signOut(IAuthService authService) async {
+    _startLoading();
+    final token = await localStore.fetch();
+
+    final result = await authService.signOut(token);
+
+    if (result.asValue!.value) {
+      localStore.delete(token);
+      emit(AuthSignOutStateSuccess());
+    } else {
+      emit(AuthStateError('Error Signing Out. Invalid Token.'));
+    }
+  }
+
+  Future signUp(ISignUpService signUpService, User user) async {
+    _startLoading();
+
+    final result = await signUpService.signUp(
+      user.email,
+      user.name,
+      user.password,
+    );
+
+    _setResultOfAuthState(result);
+  }
 
   void _setResultOfAuthState(Result<Token> result) {
     if (result.asError != null) {
